5862c53b56275f945676d1e990676ed31fe00d78
Carl Richard Steen Fosse <crsfosse@gmail.com>
2021-01-14 11:30:59 +0100

    Update main.c

--- cellular/mqtt_work/src/main.c
+++ cellular/mqtt_work/src/main.c
@@ -101,46 +101,6 @@ void bsd_recoverable_error_handler(uint32_t err)
 
 #include <soc.h>
 #include <nrfx.h>
- 
-#ifdef FIDO_TRACE
-
-void modem_trace_enable(void)
-{
-    /* GPIO configurations for trace and debug */
-    #define CS_PIN_CFG_TRACE_CLK    21 //GPIO_OUT_PIN21_Pos
-    #define CS_PIN_CFG_TRACE_DATA0  22 //GPIO_OUT_PIN22_Pos
-    #define CS_PIN_CFG_TRACE_DATA1  23 //GPIO_OUT_PIN23_Pos
-    #define CS_PIN_CFG_TRACE_DATA2  24 //GPIO_OUT_PIN24_Pos
-    #define CS_PIN_CFG_TRACE_DATA3  25 //GPIO_OUT_PIN25_Pos
- 
-    NRF_GPIO_Type *port = NRF_P0_NS;
- 
-    // Configure outputs.
-    // CS_PIN_CFG_TRACE_CLK
-    port->PIN_CNF[CS_PIN_CFG_TRACE_CLK] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-                                                (GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
- 
-    // CS_PIN_CFG_TRACE_DATA0
-    port->PIN_CNF[CS_PIN_CFG_TRACE_DATA0] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-                                                  (GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
- 
-    // CS_PIN_CFG_TRACE_DATA1
-    port->PIN_CNF[CS_PIN_CFG_TRACE_DATA1] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-                                                  (GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
- 
-    // CS_PIN_CFG_TRACE_DATA2
-    port->PIN_CNF[CS_PIN_CFG_TRACE_DATA2] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-                                                  (GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
- 
-    // CS_PIN_CFG_TRACE_DATA3
-    port->PIN_CNF[CS_PIN_CFG_TRACE_DATA3] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-                                                  (GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
- 
-    port->DIR = 0xFFFFFFFF;
- 
-}
-
-#endif
 
 /**@brief Function to print strings without null-termination
  */
